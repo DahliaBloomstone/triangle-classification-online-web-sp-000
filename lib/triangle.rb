a693c1ae25e8aa7b16938a50fd2a51ce2032c7de
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-04 02:03:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,8 @@ end
 def kind
 if (side1 <= 0 || side2 <= 0 || side3 <= 0) || (side1 >= side2 + side3 || side2 >= side1 + side3 || side3 >= side1 + side2)
 #first: test if all sides are less than 0 or = to 0
-#second: triangle inequality principle 
+#second: triangle inequality principle
+#The sum of two sides are greater than one side, continue with the method 
 begin
 raise TriangleError
 #rescue TriangleError => error
