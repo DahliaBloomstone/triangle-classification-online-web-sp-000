e528b0c983d794b73715d3c73fbbc4913facf7c2
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-04 01:56:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,17 +9,10 @@ def initialize(side1, side2, side3)
 @side2 = side2
 @side3 = side3
 end
-# kind method
-#  compute 2 validity tests
-#  if all side lengths > 0, validity test 1 = true
-#  then yield to method to test triangle inequality spec
-#  sum of 2 sides > 3rd side; if true, continue with #kind method
-#  output kind of triangle
 
+#define instance method kind that returns, as a symbol, its type:
 def kind
-      # first test for nonzero & nonnegative side lengths
-      # and test for "triangle inequality" principle
-if  (s1 <= 0 || s2 <= 0 || s3 <= 0) ||
+if (s1 <= 0 || s2 <= 0 || s3 <= 0) ||
 (s1 >= s2 + s3 || s2 >= s1 + s3 || s3 >= s1 + s2)
 begin
 raise TriangleError
@@ -34,7 +27,7 @@ elsif s1 != s2 && s2 != s3 && s1 != s3
 elsif s1 == s2 || s2 == s3 || s1 == s3
 :isosceles
 end
-end 
+end
 
 class TriangleError < StandardError
 def message
