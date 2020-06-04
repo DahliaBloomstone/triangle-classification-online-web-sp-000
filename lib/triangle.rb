79e4066f20cfbb6bac60d08db3a1d3a3ae35975f
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-04 02:05:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,15 +19,16 @@ if (side1 <= 0 || side2 <= 0 || side3 <= 0) || (side1 >= side2 + side3 || side2 
 begin
 raise TriangleError #Error if triangle inequality or negative sides or no sides
 #rescue TriangleError => error
-puts error.message #error message below 
+puts error.message #error message below
 end
 #if pass validity tests, determine kind of triangle
-elsif side1 == side2 && side2 == side3
+elsif side1 == side2 && side2 == side3 #equal sides
 :equilateral
-elsif side1 != side2 && side2 != side3 && side1 != side3
-:scalene
 elsif side1 == side2 || side2 == side3 || side1 == side3
 :isosceles
+elsif side1 != side2 && side2 != side3 && side1 != side3 #
+:scalene
+
 end
 end
 
