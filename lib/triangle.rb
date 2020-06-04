f60d6b24239355bb79746f2d9764c72cc3931f8b
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-04 01:43:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -28,6 +28,7 @@ class Triangle
   class TriangleError < StandardError
   end
 
+#define instance method valid? that returns if a triangle is valid. 
   def valid?
     sum_one_two = @sides[0] + @sides[1]
     sum_one_three = @sides[0] + @sides[2]
