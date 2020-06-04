be1b98246a8b637659269e66d9fd87b863706e9f
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-04 01:32:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,22 +1,10 @@
 class Triangle
+#Triangle class with three arguments on initialization, each argument is a length of one of three sides of a triangle. 
   def initialize(side1, side2, side3)
     @sides = []
     @sides << side1
     @sides << side2
     @sides << side3
-  end
-
-  def valid?
-    sum_one_two = @sides[0] + @sides[1]
-    sum_one_three = @sides[0] + @sides[2]
-    sum_two_three = @sides[1] + @sides[2]
-
-    if (@sides.none? {|side| side <= 0}) &&
-      (sum_one_two > @sides[2] && sum_one_three > @sides[1] && sum_two_three > @sides[0])
-      return true
-    else
-      return false
-    end
   end
 
   def kind
@@ -32,7 +20,22 @@ class Triangle
       raise TriangleError
     end
   end
-end
+  end
+
+
+
+  def valid?
+    sum_one_two = @sides[0] + @sides[1]
+    sum_one_three = @sides[0] + @sides[2]
+    sum_two_three = @sides[1] + @sides[2]
+
+    if (@sides.none? {|side| side <= 0}) &&
+      (sum_one_two > @sides[2] && sum_one_three > @sides[1] && sum_two_three > @sides[0])
+      return true
+    else
+      return false
+    end
+  end
 
 class TriangleError < StandardError
 
