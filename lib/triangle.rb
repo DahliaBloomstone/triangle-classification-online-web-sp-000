55e1fabc5b632b86d99aa46e597ff5c03b832c93
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-04 01:58:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,6 +13,7 @@ end
 #define instance method kind that returns, as a symbol, its type:
 def kind
 if (side1 <= 0 || side2 <= 0 || side3 <= 0) || (side1 >= side2 + side3 || side2 >= side1 + side3 || side3 >= side1 + side2)
+
 begin
 raise TriangleError
 #rescue TriangleError => error
