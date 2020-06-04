b128e4443f6ddddb9909efb2f186f7a82d793934
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-04 01:51:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ class Triangle
 #Triangle class with three arguments on initialization, each argument is a length of one of three sides of a triangle.
   def initialize(side1, side2, side3)
     @sides = [] #creates a new array of triangle sides
-    @sides << side1 #triangle side 1 will be shovelled into new array of triangle sides
+    @sides << side1 #triangle side 1 will be shovelled into new array of triangle sides 
     @sides << side2
     @sides << side3
   end
@@ -26,4 +26,18 @@ class Triangle
 
   #custom error class, TriangleError that inherits from StandardError
   class TriangleError < StandardError
+  end
+
+#define instance method valid? that returns if a triangle is valid.
+  def valid?
+    sum_one_two = @sides[0] + @sides[1]
+    sum_one_three = @sides[0] + @sides[2]
+    sum_two_three = @sides[1] + @sides[2]
+
+    if (@sides.none? {|side| side <= 0}) &&
+      (sum_one_two > @sides[2] && sum_one_three > @sides[1] && sum_two_three > @sides[0])
+      return true
+    else
+      return false
+    end
   end
